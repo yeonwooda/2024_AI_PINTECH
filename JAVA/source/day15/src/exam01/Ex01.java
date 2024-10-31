02b42f1470043d8122c6de9cc35b124320b52ed1
yeonwooda <mcsun5@naver.com>
2024-10-31 17:24:39 +0900

    day15 수업 소스

---
+++ JAVA/source/day15/src/exam01/Ex01.java
@@ -0,0 +1,19 @@
+package exam01;
+
+import java.util.ArrayList;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        ArrayList<String> items = new ArrayList<>();
+        items.add("항목1");
+        items.add("항목2");
+        items.add("항목3");
+        items.add("항목4");
+        items.add("항목5");
+
+        for (String item : items) {
+            System.out.println(item);
+        }
+    }
+
+}
