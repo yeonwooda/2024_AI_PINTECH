05782f1daa9849b3ee8672b9a86111346d6b899a
yeonwooda <mcsun5@naver.com>
2024-11-05 09:51:06 +0900

    day17 수업 소스

---
+++ JAVA/source/day17/src/exam01/Ex03.java
@@ -0,0 +1,22 @@
+package exam01;
+
+import java.util.Collections;
+
+public class Ex03 {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 4, 5, 6};
+        int[] nums2 = {1, 2, 3, 4, 5, 6};
+
+        boolean isSame = true;
+        for (int i = 0; i < nums1.length; i++) {
+            int num1 = nums1[i];
+            int num2 = nums2[i];
+            if (num1 != num2) {
+                isSame = false;
+                break;
+            }
+
+        }
+        System.out.println(isSame);
+    }
+}
