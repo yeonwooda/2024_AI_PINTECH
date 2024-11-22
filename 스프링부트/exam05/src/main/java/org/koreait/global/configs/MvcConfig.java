7d7ac14f189ec7ee2495940d150bbe9865e7573e
yeonwooda <mcsun5@naver.com>
2024-11-22 18:06:31 +0900

    2024.11.22

--- 스프링부트/exam05/src/main/java/org/koreait/global/configs/MvcConfig.java
+++ 스프링부트/exam05/src/main/java/org/koreait/global/configs/MvcConfig.java
@@ -1,6 +1,10 @@
 package org.koreait.global.configs;
 
+import org.koreait.etc.Greet;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,6 +15,7 @@ public class MvcConfig implements WebMvcConfigurer {
 
     /**
      * 모든 @Valid가 적용된 커맨드 객체 검증에 사용
+     *
      * @return
      */
     /*
@@ -20,4 +25,24 @@ public class MvcConfig implements WebMvcConfigurer {
     }
 
      */
+    @Bean
+    @Profile("prod")
+    @Primary // 빈이 충돌될때 가장 먼저 선택
+    public Greet greetProd() {
+        System.out.println("prod profile");
+        Greet greet = new Greet();
+        greet.setName("prod에서");
+        return greet;
+    }
+
+    @Bean
+    @Profile("dev")
+    public Greet greetDev() {
+        System.out.println("dev profile");
+        Greet greet = new Greet();
+        greet.setName("dev에서");
+
+        return greet;
+    }
+
 }
