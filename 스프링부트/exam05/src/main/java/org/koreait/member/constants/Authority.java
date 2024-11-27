b20e3665ab4c1ccfb0b95180a8981545119333ca
yeonwooda <mcsun5@naver.com>
2024-11-27 16:53:50 +0900

    2024.11.27

---
+++ 스프링부트/exam05/src/main/java/org/koreait/member/constants/Authority.java
@@ -0,0 +1,7 @@
+package org.koreait.member.constants;
+
+public enum Authority {
+    USER, // 일반 관리자
+    MANAGER, // 부관리자
+    ADMIN // 최고 관리가
+}
---
+++ 스프링부트/exam05/src/main/java/org/koreait/member/repositories/MemberRepository.java
@@ -0,0 +1,14 @@
+package org.koreait.member.repositories;
+
+import org.koreait.member.entities.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    // Member findByEmail(String email);
+    Optional<Member> findByEmail(String email);
+
+
+}
