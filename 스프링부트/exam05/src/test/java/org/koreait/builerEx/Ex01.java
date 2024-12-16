f657d12e57e9f7d0a8b7c7aea106cdda0cdf35de
yeonwooda <mcsun5@naver.com>
2024-12-16 14:30:48 +0900

    .

--- 스프링부트/exam05/src/test/java/org/koreait/builerEx/Ex01.java
+++ 스프링부트/exam05/src/test/java/org/koreait/builerEx/Ex01.java
@@ -1,4 +1,23 @@
 package org.koreait.builerEx;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles({"default", "test"})
 public class Ex01 {
+
+    @Test
+    void test1() {
+        User user = User.builder()
+                .email("user01@test.org")
+                .password("12345678")
+                .name("이이름")
+                .build();
+
+
+        System.out.println(user);
+    }
+
 }
