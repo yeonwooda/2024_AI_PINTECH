ae9176ecc9653c2e77562060a378916fbe170add
yeonwooda <mcsun5@naver.com>
2025-01-06 11:31:45 +0900

    /

---
+++ 금융 OpenAPI 활용/exam01/src/main/java/org/koreait/member/controllers/MemberController.java
@@ -0,0 +1,15 @@
+package org.koreait.member.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+    @PostMapping("/join")
+    public void joinPs(RequestJoin form) {
+        System.out.println(form);
+    }
+}
